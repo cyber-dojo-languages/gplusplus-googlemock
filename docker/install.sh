57145907f36cedb2f8f7ab19eb6f5d93a00ccfbb
Jon Jagger <jon@jaggersoft.com>
2018-02-09 13:32:11 +0000

    fixed broken build by COPYing gmock zip file into image rather than wget'ing from URL that no longer works

--- docker/install.sh
+++ docker/install.sh
@@ -8,8 +8,6 @@ cd /usr/src
 apt-get update
 apt-get install --yes wget cmake unzip
 
-#WAS wget http://googlemock.googlecode.com/files/gmock-1.7.0.zip
-wget http://pkgs.fedoraproject.org/repo/pkgs/gmock/gmock-${GMOCK_VERSION}.zip/073b984d8798ea1594f5e44d85b20d66/gmock-${GMOCK_VERSION}.zip
 unzip gmock-${GMOCK_VERSION}.zip
 
 cd /usr/src/gmock-${GMOCK_VERSION}
