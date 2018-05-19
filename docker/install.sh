44ddaa0c8f7808f13d25d906a03351b6f7e7eb13
Jon Jagger <jon@jaggersoft.com>
2018-05-19 14:21:35 +0100

    refactoring; delete unneeded wget from install.sh

--- docker/install.sh
+++ docker/install.sh
@@ -4,7 +4,7 @@ set -e
 readonly GTEST_VERSION=1.8.0
 
 apt-get update
-apt-get install --yes wget cmake unzip
+apt-get install --yes cmake unzip
 
 cd /usr/src
 unzip googletest-release-${GTEST_VERSION}.zip
@@ -16,4 +16,4 @@ mv googlemock/gtest/libg* /usr/lib
 cp -rf googlemock/include/gmock /usr/include
 cp -rf googletest/include/gtest /usr/include
 
-apt-get remove --yes wget cmake unzip
+apt-get remove --yes cmake unzip
