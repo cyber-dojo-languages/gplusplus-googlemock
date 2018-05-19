9f50a0b0857c3df407132958ad7b7102e8cbf423
Jon Jagger <jon@jaggersoft.com>
2018-05-19 14:19:44 +0100

    upgraded to gmock 1.8.0

--- docker/install.sh
+++ docker/install.sh
@@ -1,23 +1,19 @@
 #!/bin/bash
 set -e
 
-readonly GMOCK_VERSION=1.7.0
-
-cd /usr/src
+readonly GTEST_VERSION=1.8.0
 
 apt-get update
 apt-get install --yes wget cmake unzip
 
-unzip gmock-${GMOCK_VERSION}.zip
-
-cd /usr/src/gmock-${GMOCK_VERSION}
-cmake DCMAKE_CXX_FLAGS='-Wno-unused-local-typedefs' .
+cd /usr/src
+unzip googletest-release-${GTEST_VERSION}.zip
+cd /usr/src/googletest-release-${GTEST_VERSION}
+cmake .
 make
-mv libg* /usr/lib
-cp -rf include/gmock /usr/include
-
-cd /usr/src/gmock-${GMOCK_VERSION}/gtest
-mv libg* /usr/lib
-cp -rf include/gtest /usr/include
+mv googlemock/libg* /usr/lib
+mv googlemock/gtest/libg* /usr/lib
+cp -rf googlemock/include/gmock /usr/include
+cp -rf googletest/include/gtest /usr/include
 
 apt-get remove --yes wget cmake unzip
