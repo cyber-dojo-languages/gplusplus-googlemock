929809e6a374984b3a74266954794b6eaf3bc2d3
Jon Jagger <jon@jaggersoft.com>
2018-09-24 18:17:55 +0200

    update starting-point files to have example of MOCK_ macro

--- start_point/hiker.hpp
+++ start_point/hiker.hpp
@@ -1,6 +1,16 @@
 #ifndef HIKER_INCLUDED
 #define HIKER_INCLUDED
 
-int answer();
+class Logger;
+
+class Hiker
+{
+public:
+    Hiker();
+    explicit Hiker(Logger &);
+    int answer();
+private:
+    Logger & logger;
+};
 
 #endif
