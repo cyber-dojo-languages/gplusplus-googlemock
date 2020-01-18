4e3314ad3a0c734be25814ffc7d051ae44eefc8b
Jon Jagger <jon@jaggersoft.com>
2020-01-18 18:43:44 +0000

    Switch to circleci; upgrade to 1.10.0

---
+++ docker/install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -Eeu
+
+readonly GTEST_VERSION=1.10.0
+
+apt-get update
+apt-get install --yes cmake unzip
+
+cd /usr/src
+unzip googletest-release-${GTEST_VERSION}.zip
+cd /usr/src/googletest-release-${GTEST_VERSION}
+cmake .
+make
+
+apt-get remove --yes cmake unzip
+mv lib/libg* /usr/lib
+cp -rf googlemock/include/gmock /usr/include
+cp -rf googletest/include/gtest /usr/include
